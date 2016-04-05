934705a6608ccf20615672a9003f6280e25ba6f3
mafer.564 <madelin_fernanda@hotmail.com>
2016-04-04 21:24:38 -0500

    agregue home

--- azul/azul/settings.py
+++ azul/azul/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'azul_app',
+    'home',
 ]
 
 MIDDLEWARE_CLASSES = [
--- azul/azul/urls.py
+++ azul/azul/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from home.views import HomeView
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^home/$',
+    	HomeView.as_view(),
+    	name="home"),
 ]
