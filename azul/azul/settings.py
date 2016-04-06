5b2c3dc955d31a7a61e144c614b54c487e4e9269
mafer.564 <madelin_fernanda@hotmail.com>
2016-04-05 21:36:40 -0500

    home chido

--- azul/azul/settings.py
+++ azul/azul/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'x_z5%$8%krq!s@m_x^ae%*9j87@if(hc(veyesjaf9d04i7d2m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'azul_app',
-    'home',
+    
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'azul.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- azul/azul/urls.py
+++ azul/azul/urls.py
@@ -15,12 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from home.views import HomeView
+from django.views.generic import TemplateView
+
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^home/$',
-    	HomeView.as_view(),
-    	name="home"),
+    url(r'^$', TemplateView.as_view(template_name='home.html')),
 ]
