0a1b895546f495ab6f3b2e56a0040985cf358466
Uriel Miranda <urielnovoa@gmail.com>
2016-04-07 19:55:50 -0500

    merge

--- azul/azul/settings.py
+++ azul/azul/settings.py
@@ -37,15 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-<<<<<<< HEAD
-
     #apps
     'azuldirectorio',
     'galery',
-=======
     'azul_app',
-    
->>>>>>> ef20a4b5096db7e2600025a10f58864ac03b3516
+
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -64,13 +60,9 @@ ROOT_URLCONF = 'azul.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-<<<<<<< HEAD
         'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
         ],
-=======
-        'DIRS': ['templates'],
->>>>>>> ef20a4b5096db7e2600025a10f58864ac03b3516
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
